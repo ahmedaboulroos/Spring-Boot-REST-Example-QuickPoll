49535d12f230382c7c15f67918743246521003fc
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-14 03:29:28 +0200

    added project DTOs

---
+++ src/main/java/io/amin/dto/OptionCount.java
@@ -0,0 +1,14 @@
+package io.amin.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OptionCount {
+
+    private int optionId;
+
+    private int count;
+
+}
---
+++ src/main/java/io/amin/dto/VoteResult.java
@@ -0,0 +1,12 @@
+package io.amin.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class VoteResult {
+
+    private int totalVotes;
+
+}
