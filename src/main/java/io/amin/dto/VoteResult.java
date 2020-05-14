2c311fe2141456068a791b27746f73ed002a30fb
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-14 05:32:58 +0200

    added results of vote DTO

--- src/main/java/io/amin/dto/VoteResult.java
+++ src/main/java/io/amin/dto/VoteResult.java
@@ -3,10 +3,14 @@ package io.amin.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
+
 @Data
 @NoArgsConstructor
 public class VoteResult {
 
     private int totalVotes;
+
+    private Collection<OptionCount> results;
 
 }
