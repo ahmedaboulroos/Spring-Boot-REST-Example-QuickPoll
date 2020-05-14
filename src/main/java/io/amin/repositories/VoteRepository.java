8d9cd284d3576216234e505e9ce939087f3d588a
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-14 05:34:01 +0200

    added method to retrieve votes based on the poll id

--- src/main/java/io/amin/repositories/VoteRepository.java
+++ src/main/java/io/amin/repositories/VoteRepository.java
@@ -1,10 +1,14 @@
 package io.amin.repositories;
 
 import io.amin.entities.Vote;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends CrudRepository<Vote, Integer> {
+
+    @Query(value = "SELECT v.* FROM options o, votes v WHERE o.poll_id = ?1 AND v.option_id = o.option_id", nativeQuery = true)
+    Iterable<Vote> findByPollId(int pollId);
 
 }
