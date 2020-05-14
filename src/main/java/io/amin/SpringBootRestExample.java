b95fbe42bb6a14efb5934544fa0dc6c45417d8af
Ahmed Aboulroos <ahmedaboulroos@gmail.com>
2020-05-14 03:28:25 +0200

    added empty spring boot project files

---
+++ src/main/java/io/amin/SpringBootRestExample.java
@@ -0,0 +1,13 @@
+package io.amin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestExample {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestExample.class, args);
+	}
+
+}
